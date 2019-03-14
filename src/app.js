540005f2f851576af7b7541b3b3f5878f84d2476
Dave Krejci <taavetti.krejci@seznam.cz>
2019-03-14 11:09:16 +0100

    Added routes/views in app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,47 @@
 // Add module dependencies
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+const path = require('path');
+// const morgan = require('morgan');
+const dashboard = require('./routes/dashboard.js');
+const login = require('./routes/login.js');
+const register = require('./routes/register.js');
+const upload = require('./routes/upload.js');
+const logs = require('./routes/logs.js');
+const users = require('./routes/users.js');
 const { sequelize } = require('./models');
 const config = require('./config/config');
+const exphbs = require('express-handlebars');
+
 
 // Configure middleware
-app.use(express.json());
-app.use(morgan('combined'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(morgan('combined'));
 
-// Connect to database
-sequelize.sync()
+// Set templating engine
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/views'));
+
+// Define routes
+app.use('/', dashboard);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/upload', upload);
+app.use('/logs', logs);
+app.use('/users', users);
+
+// Connect to database and sync models
+sequelize.sync({force: true})
     .then(() => {
-        console.log('Connection to database successfully established');        
+        console.log('Connection to database successfully established');  
+        
+        // Start server
+        app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
+
     }).catch((err) => {
         console.log('Error connecting to the database:', err);
     });
 
-// Start server
-app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
 
