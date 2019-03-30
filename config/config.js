b30e3643823fa8d4ba0b74bbf9d67ddd7f7de9fe
Dave Krejci <taavetti.krejci@seznam.cz>
2019-03-30 20:45:21 +0100

    swapped mysql for postgres

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 let config = {
     port: process.env.PORT || 3000,
     db: {
-        database: process.env.DB_NAME || 'torque-dash',
-        user: process.env.DB_USER || 'dave',
+        database: process.env.DB_NAME || 'torquedash',
+        user: process.env.DB_USER || 'postgres',
         password: process.env.DB_PASS || 'heslo',
         options: {
-            dialect: process.env.DIALECT || 'mysql',
+            dialect: process.env.DIALECT || 'postgres',
             host: process.env.HOST || 'localhost',
             operatorsAliases: false,
             logging: true
