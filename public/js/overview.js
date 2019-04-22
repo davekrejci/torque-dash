2a3aaee1ff4a2f027ae519ff214cdcfe35ad883c
Dave <taavetti.krejci@seznam.cz>
2019-04-22 15:45:41 +0200

    added csv export

--- public/js/overview.js
+++ public/js/overview.js
@@ -1,41 +1,122 @@
 /* eslint-disable */
+let overviewModule = {
 
-// On page load
-$(document).ready(function () {
-    // Initialize datatable
-    $('#logTable').DataTable({
-        "dom": '<f<t><"my-3"i><"my-3"p>>',
-        "bLengthChange": false,
-        "pageLength": 5,
-        responsive: true,
-        ajax: {
-            url: '/api/sessions',
-            dataSrc: ''
-        },
-        columns: [
-            { data: 'name' },
-            { data: 'startDate' },
-            { data: 'endDate' },
-            { data: 'duration' },
-            { data: 'startLocation' },
-            { data: 'endLocation' },
-            { data: null },
-        ],
-        columnDefs: [
-            {
-                // puts buttons in the last column
-                targets: [-1], render: function (data, type, row, meta) {
-                    return `<a data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-primary table-button rounded primary-tooltip" href="/edit/${data.id}"><i class="fas fa-pen"></i></a>
-                    <span data-toggle="tooltip" data-placement="top" title="Delete">
-                    <button class="btn btn-primary table-button rounded primary-tooltip" data-toggle="modal" data-target="#deleteSessionModal"
-                        data-id="${data.id}"><i class="fas fa-trash"></i></button>
-                    </span>`
-                }
-        }],
-        order: [ 1, "desc" ],
-    });
-});
+    init: async function() {
+        this.cacheDOM();
+        this.bindEvents();
+        this.initLogTable();
+    },
+    cacheDOM: function() {
+        this.$loadOverlay = $('#loadOverlay').hide();
+        this.$logTable = $('#logTable');
+    },
+    bindEvents: function() {
+        $(document).ajaxStart( this.showLoadOverlay.bind(this) );
+        $(document).ajaxStop( this.hideLoadOverlay.bind(this) );
+        $('#deleteSessionModal').on('show.bs.modal', function (event) {
+            let button = $(event.relatedTarget) // Button that triggered the modal
+            let id = button.data('id') // Extract info from data-* attributes
+            let modal = $(this)
+            modal.find('.modal-footer #modalDeleteButton').attr("onclick", "overviewModule.deleteSession('"+id+"')");
+        });
+    },
+    initLogTable: async function() {
+        this.$logTable.DataTable({
+            "dom": '<f<t><"my-3"i><"my-3"p>>',
+            "bLengthChange": false,
+            "pageLength": 5,
+            responsive: true,
+            ajax: {
+                url: '/api/sessions',
+                dataSrc: ''
+            },
+            columns: [
+                { data: 'name' },
+                { data: 'startDate' },
+                { data: 'endDate' },
+                { data: 'duration' },
+                { data: 'startLocation' },
+                { data: 'endLocation' },
+                { data: null },
+            ],
+            columnDefs: [
+                {
+                    // puts buttons in the last column
+                    targets: [-1], render: function (data, type, row, meta) {
+                        return `<a data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-primary table-button rounded primary-tooltip" href="/edit/${data.id}"><i class="fas fa-pen"></i></a>
+                        <span data-toggle="tooltip" data-placement="top" title="Delete">
+                        <button class="btn btn-primary table-button rounded primary-tooltip" data-toggle="modal" data-target="#deleteSessionModal"
+                            data-id="${data.id}"><i class="fas fa-trash"></i></button>
+                        </span>
+                        <span data-toggle="tooltip" data-placement="top" title="Export">
+                            <button class="btn btn-primary table-button rounded primary-tooltip exportButton" onclick="overviewModule.exportCSV(${data.id})" data-id="${data.id}"><i class="fas fa-download"></i></button>
+                        </span>`
+                    }
+            }],
+            order: [ 1, "desc" ],
+        });
+    },
+    showLoadOverlay: function() {
+        this.$loadOverlay.show();
+    },
+    hideLoadOverlay: function() {
+        this.$loadOverlay.hide();
+    },
+    deleteSession: async function(id) {
+        try{
+            await Session.deleteSession(id);
+            this.$logTable.DataTable().ajax.reload();
+        }
+        catch(err) {
+            console.log(err);
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong! Please try again.'
+            });
+        }
+    },
+    exportCSV: async function(id) {
+        try{
+            // Get session
+            let session = await Session.getSession(id);
+            let exportedFileName = session.name;
+            // Create list of all columns
+            let columns = ['timestamp','lon','lat'];
+            let allValues = new Array;
+            session.Logs.forEach(log => {
+                allValues.push(Object.keys(log.values));
+            });
+            let valueSet = [...new Set([].concat(...allValues))];
+            valueSet.forEach(value => {
+                columns.push(`values.${value}`);
+            });
+            // Parse json to csv
+            const json2csvParser = new json2csv.Parser({  defaultValue: '-', fields: columns });
+            const csv = json2csvParser.parse(session.Logs);
+            // Download csv
+            var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+            var link = document.createElement("a");
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", `${exportedFileName}.csv`);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+        catch (err) {
+            console.log(err);
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong! Please try again.'
+            });
+        }
+    }
+}
 
+overviewModule.init();
     
     
     
